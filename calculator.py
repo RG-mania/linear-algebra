aa67a6358c3605ca37033fc740bb9895a7664519
Roy G <roy.gotlieb@gmail.com>
2021-03-22 14:12:28 -0700

    added implementation of Bareiss algorithm to find determinant of any size matrix

--- calculator.py
+++ calculator.py
@@ -7,11 +7,20 @@ def det2x2(arr):
     else:
         raise NameError('Array is not 2x2')
 
-
+def detBareiss(arr):
+    pivot = 1
+    arrsize = len(arr)
+    for k in range(arrsize-1):
+        for i in range(k+1, arrsize):
+            for j in range(k+1, arrsize):
+                #print("k, i, j: ", k, i, j)
+                arr[i][j] = (arr[i][j]*arr[k][k] - arr[i][k]*arr[k][j])/pivot
+        pivot = arr[k][k]
+    return arr[arrsize-1][arrsize-1]
 
 def main():
-    arr = [[1, 2], [3, 4]]
-    print(det2x2(arr))
+    arr = [[1, 1, 1], [2, -3, 3], [1, 2, 2]]
+    print(detBareiss(arr))
 
 if __name__ == "__main__":
     main()
