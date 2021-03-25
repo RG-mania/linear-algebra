542fbf914d7901fc1ea8c2b4f3367e1765c16c69
Roy G <roy.gotlieb@gmail.com>
2021-03-24 17:24:22 -0700

    added some basic tests, removed unnecessary swapping for detBareiss

--- calculator.py
+++ calculator.py
@@ -7,6 +7,7 @@
 #Find inverse of matrix
 #Find eigenvalues / eigenvectors of 2x2 and 3x3 matrices (if they exist)
 
+"""Assumes matrix is 2x2"""
 def det2x2(arr):
     if(len(arr) == 2 and len(arr[0]) == 2 and len(arr[1]) == 2):
         det = arr[0][0]*arr[1][1] - arr[0][1]*arr[1][0]
@@ -25,24 +26,26 @@ def detBareiss(arr):
         for i in range(k+1, arrsize):
             for j in range(k+1, arrsize):
                 #print("k, i, j: ", k, i, j)
-                arr[i][j] = (arr[i][j]*arr[k][k] - arr[i][k]*arr[k][j])/pivot
+                arr[i][j] = (arr[i][j]*arr[k][k] - arr[i][k]*arr[k][j])//pivot
         pivot = arr[k][k]
         p = k+2
         while (arr[k+1][k+1] == 0 and p<arrsize):
-            print('swapping rows')
-            temp = arr[k+1]
-            arr[k+1] = arr[p]
-            arr[p] = temp
-            print(arr[k+1])
-            print(arr[p])
-            negmult *= -1
-            p+=1
+            #print('swapping rows')
+            if (arr[p][k+1] != 0):
+                temp = arr[k+1]
+                arr[k+1] = arr[p]
+                arr[p] = temp
+                # print(arr[k+1])
+                # print(arr[p])
+                negmult *= -1
+            else:
+                p+=1
 
     return arr[arrsize-1][arrsize-1]*negmult
 
 """Note: The following method assumes that the number of equations is equal to the number of variables"""
 #Edit: it might not - still have to test
-def sysEquationsBareiss(arr):
+def linearSystem(arr):
     divisor = 1
     arrsize = len(arr) #number of equations
     numvars = len(arr[0]) - 1
@@ -53,7 +56,7 @@ def sysEquationsBareiss(arr):
                 for j in range(numvars+1):
                     if j != k+poffset:
                         #print(arr[i][k + poffset])
-                        arr[i][j] = (arr[i][j]*arr[k][k+poffset] - arr[i][k+poffset]*arr[k][j])/divisor
+                        arr[i][j] = (arr[i][j]*arr[k][k+poffset] - arr[i][k+poffset]*arr[k][j])//divisor
                         #print('arr[{i}][{j}] = {val}'.format(i=i, j=j, val = arr[i][j]))
         for l in range(arrsize):
             if l != k:
@@ -104,13 +107,13 @@ def sysEquationsBareiss(arr):
             finVector.append(ans/div)
     return finVector
 
-def main():
-    arr = [[1, -4, 1, 2], [-1, 4, 4, 1], [3, 3, 3, 4], [2, 5, 2, -1]]
-    arr2 = [[1, 1, 1], [2, -3, 3], [1, 2, 2]]
-    sysEq = [[1, 1, 1, 3], [2, -3, 3, 8], [1, 2, 2, 5]]
-    badSys = [[1, 1, 1, 3], [1, 1, 1, 4], [1, 2, 2, 5]]
-    # print(detBareiss(arr2))
-    print(sysEquationsBareiss(badSys))
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     arr = [[1, -4, 1, 2], [-1, 4, 4, 1], [3, 3, 3, 4], [2, 5, 2, -1]]
+#     arr2 = [[1, 1, 1], [2, -3, 3], [1, 2, 2]]
+#     sysEq = [[1, 1, 1, 3], [2, -3, 3, 8], [1, 2, 2, 5]]
+#     badSys = [[1, 1, 1, 3], [1, 1, 1, 4], [1, 2, 2, 5]]
+#     # print(detBareiss(arr2))
+#     print(linearSystem(badSys))
+#
+# if __name__ == "__main__":
+#     main()
